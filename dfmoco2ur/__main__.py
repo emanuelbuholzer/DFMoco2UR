7baaa83d167239ba40eae48c1197f439ebf2afeb
Emanuel Buholzer <emanuel0xb@gmail.com>
2020-09-14 10:57:37 +0200

    Logging via configuration and drop CLI support

--- dfmoco2ur/__main__.py
+++ dfmoco2ur/__main__.py
@@ -16,25 +16,30 @@ if __name__ == '__main__':
     default_config_path = base_path.joinpath('dfmoco-ur', 'config.yaml')
 
     # Parse CLI arguments to obtain the config and log level
-    parser = argparse.ArgumentParser(description='Dragonframe Motion Control to Universal Robot bridge', prefix_chars='-')
-    parser.add_argument('--config', default=default_config_path, help='Path to a configuration')
-    parser.add_argument('--logLevel', type=int, default=10, help='Log level according to the python logging module')
+    parser = argparse.ArgumentParser(
+        description='Dragonframe Motion Control to Universal Robot bridge', prefix_chars='-')
+    parser.add_argument('--config', default=default_config_path,
+                        help='Path to a configuration')
     args = parser.parse_args()
 
-    # Setup logging
-    # TODO: We might want to send our logs out via a Websocket.
-    logging.basicConfig(level=args.logLevel)
-    logging.getLogger('ursecmon').setLevel(logging.INFO)
-    logging.getLogger('urx').setLevel(logging.ERROR)
-
     # Parse the configuration and run the scheduler
-    if args.config == default_config_path:
-        logging.info(f"Using default configuration from {args.config}")
-    else:
-        logging.debug(f"Using custom configuration from {args.config}")
     try:
         with open(args.config, 'r') as raw_config:
             config = yaml.load(raw_config, Loader=yaml.FullLoader)
+
+            # Setup logging
+            # TODO: We might want to send our logs out via a Websocket.
+            for logger in config['logging']:
+                if logger == "dfmoco2ur":
+                    logging.basicConfig(level=config['logging'][logger])
+                else:
+                    logging.getLogger(logger).setLevel(config['logging'][logger])
+
+            if args.config == default_config_path:
+                logging.info(f"Using default configuration from {args.config}")
+            else:
+                logging.debug(f"Using custom configuration from {args.config}")
+
             asyncio.run(bridge.run(config))
     except FileNotFoundError as err:
         logging.critical(err)
