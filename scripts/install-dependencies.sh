931f1e5bb067687a550826d3345487c3d3f55868
Emanuel Buholzer <emanuel0xb@gmail.com>
2020-10-22 17:10:56 +0200

    Make first attempts to go into a mono repo

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-source venv/bin/activate
+# Setup dfmoco2ur-api
+
 
 git submodule init
 git submodule update
 
+pushd dfmoco2ur-api
+source venv/bin/activate
 pip3 install -r requirements.txt
+popd
+deactivate
---
+++ scripts/run-tests.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pushd dfmoco2ur-api
+source venv/bin/activate
+python3 -m pytest
+popd
+deactivate
